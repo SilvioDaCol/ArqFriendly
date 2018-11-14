7d2f87cb0c61aab0f159d43018e1bbf998cfacd4
phbelucci <phbelucci@gmail.com>
2018-11-14 18:03:30 -0200

    telas novas

--- Documentacao/Prototipacao/gameLearningProject/gameLearning/telaInicial.cs
+++ Documentacao/Prototipacao/gameLearningProject/gameLearning/telaInicial.cs
@@ -34,6 +34,11 @@ namespace gameLearning
 
         }
 
-        
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Close();
+            Form login = new Form();
+            Application.Run(login);
+        }
     }
 }
