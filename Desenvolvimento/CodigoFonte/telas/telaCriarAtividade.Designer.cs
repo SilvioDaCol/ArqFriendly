03916ade4e93b741d47ee63de2fe043b6703742d
phbelucci <phbelucci@gmail.com>
2018-11-23 23:34:02 -0200

    telas faltantes... falta adicionar elementos

---
+++ Desenvolvimento/CodigoFonte/telas/telaCriarAtividade.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace gameLearning.telas
+{
+    partial class telaCriarAtividade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // telaCriarAtividade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gameLearning.Properties.Resources.mainBack;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1182, 628);
+            this.MaximumSize = new System.Drawing.Size(1200, 675);
+            this.MinimumSize = new System.Drawing.Size(1200, 675);
+            this.Name = "telaCriarAtividade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "telaCriarAtividade";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
