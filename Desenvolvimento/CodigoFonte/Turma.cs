4dccbad751cb1820b716af187e5ed70355fa135f
SilvioDaCol <silviocbrito@hotmail.com>
2018-11-30 16:30:08 -0200

    Padronizacao tabelas, ajustes nos jogos

--- Desenvolvimento/CodigoFonte/Turma.cs
+++ Desenvolvimento/CodigoFonte/Turma.cs
@@ -68,6 +68,10 @@ namespace gameLearning
         {
             Conexao banco = new Conexao();
             string resposta = banco.deletaTurma(cod_turma);
+            if(resposta.StartsWith("ERRO: "))
+            {
+                resposta = "Não foi possível deletar turma, pois há alunos matriculados";
+            }
             return resposta;
         }
     }
